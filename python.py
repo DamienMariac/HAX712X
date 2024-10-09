b83ad553df4a1ac6acc7f6932c2c38fa0fba69b9
Damien <damien.mariac1@gmail.com>
2024-10-09 10:50:48 +0200

    maj

--- python.py
+++
@@ -1,3 +0,0 @@
-#Le but est d'extraire les données pour pouvoir les manipuler ET les afficher sur une map
-
-# le but est donc d'apprendre à utiliser (importer) une carte et d'extraire automatiquement des données depuis un site
