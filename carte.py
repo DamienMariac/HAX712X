17f5c845bd7cead21e118c378f6744a5e81cd551
Damien <damien.mariac1@gmail.com>
2024-10-16 20:09:18 +0200

    map + test between 2 loc

--- carte.py
+++ carte.py
@@ -1,10 +1,23 @@
 #%%
-import folium
-import json
+import osmnx as ox
+import networkx as nx
+import matplotlib.pyplot as plt
 
-Montpellier = folium.Map(location = [43.62505, 3.862038], zoom_start = 12)
-Montpellier
+ox.config(log_console=True, use_cache=True)
 
+place_name = "Montpellier, France"
 
+graph = ox.graph_from_place(place_name, network_type='walk')
+
+location_1 = ox.geocode("012 Boutonnet, Montpellier, France")
+location_2 = ox.geocode("057 Saint-Guilhem, Montpellier, France")
+
+node_start = ox.distance.nearest_nodes(graph, X=location_1[1], Y=location_1[0])
+node_end = ox.distance.nearest_nodes(graph, X=location_2[1], Y=location_2[0])
+
+route = nx.shortest_path(graph, node_start, node_end, weight='length')
+
+fig, ax = ox.plot_graph_route(graph, route, node_size=0)
+plt.show()
 
 # %%
--- extract.py
+++ extract.py
@@ -1,52 +1,14 @@
-import os
-import pooch
-from bs4 import BeautifulSoup
-import requests
+#%%
+import pandas as pd
 
-url = 'https://data.montpellier3m.fr/dataset/courses-des-velos-velomagg-de-montpellier-mediterranee-metropole'
+###############################################################################
 
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
+data = pd.read_csv('./data/TAM_MMM_CoursesVelomagg.csv') #début / arrivé
 
-#on soupe
+selected_data = data[['Departure station', 'Return station']]
 
-# json_links = []
-# for link in soup.find_all('a', href=True):
-#     href = link['href']
-#     if href.endswith('.json'):
-#         json_links.append(href)
+output_path = './data/selected_stations.csv'
+selected_data.to_csv(output_path, index=False)
 
-
-csv_list = []
-for link in soup.find_all('a', href=True):
-    href = link['href']
-    if href.endswith('.csv'):
-        csv_list.append(href)
-
-
-
-
-data_folder = './data'
-
-#on supp
-
-for file in os.listdir(data_folder):
-    file_path = os.path.join(data_folder, file)
-    try:
-        if os.path.isfile(file_path) or os.path.islink(file_path):
-            os.unlink(file_path)  # Supprimer les fichiers ou liens symboliques
-            print(f"Fichier supprimé : {file_path}")
-    except Exception as e:
-        print(f"Erreur en supprimant {file_path} : {e}")
-
-
-#on ajoute
-
-for url in csv_list:
-    file_name = url.split('/')[-1]
-    file_path = pooch.retrieve(
-        url=url,
-        known_hash=None, 
-        fname=file_name, 
-        path='./data'
-    )
+###############################################################################
+# %%
