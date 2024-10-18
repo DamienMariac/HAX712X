dc92a3a107746400f2f8bbc0281e99df4d62c6df
Damien <damien.mariac1@gmail.com>
2024-10-18 10:30:00 +0200

    map files

--- map/carte.py
+++ map/carte.py
@@ -16,5 +16,5 @@ node_end = ox.distance.nearest_nodes(graph, X=location_2[1], Y=location_2[0])
 route = nx.shortest_path(graph, node_start, node_end, weight='length')
 
 m = ox.plot_route_folium(graph, route, route_color='red', route_opacity=0.8, route_weight=5)
-m.save('route_map.html')
+m.save('./map/route_map.html')
 m  
