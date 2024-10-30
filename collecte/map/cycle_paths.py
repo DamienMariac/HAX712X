4e682d732d241c858caa4ac5267b288c4d6615aa
Marine Germain <marine.germain@etu.umontpellier.fr>
2024-10-30 12:17:13 +0100

    fix typo

--- collecte/map/cycle_paths.py
+++ collecte/map/cycle_paths.py
@@ -8,17 +8,13 @@ import osmnx as ox
 csv_path = '/home/marinegermain/PROJET/collecte/data/active_bike_paths.csv'  
 df = pd.read_csv(csv_path)
 
-# Filtrer les données (si nécessaire)
-df_filtered = df[df['statut_d'] == 'actif']  # Ajuste selon tes critères
-osm_ids = df_filtered['id_osm'].unique()
-
 # Récupérer les géométries pour toutes les pistes cyclables
-gdf = ox.geometries_from_place('Montpellier, France', tags={'highway': 'cycleway'})
+gdf = ox.features.features_from_place('Montpellier, France', tags={'highway': 'cycleway'})
 
 # Créer une carte centrée sur Montpellier
 m = folium.Map(location=[43.6115, 3.8772], zoom_start=13)
 
-# Ajouter chaque piste cyclable à la carte avec une couleur rouge
+# Ajouter chaque piste cyclable à la carte en rouge
 for _, row in gdf.iterrows():
     folium.GeoJson(
         row['geometry'],
@@ -30,5 +26,4 @@ for _, row in gdf.iterrows():
         }
     ).add_to(m)
 
-# Enregistrer la carte en tant que fichier HTML
 m.save('/home/marinegermain/PROJET/collecte/map/map_cycle_paths.html')
