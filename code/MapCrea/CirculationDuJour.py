9988a82ff51efa23bab55d023fb3a6a9375c1004
Damien <damien.mariac1@gmail.com>
2024-12-05 17:21:29 +0100

    map fix

--- code/MapCrea/CirculationDuJour.py
+++ code/MapCrea/CirculationDuJour.py
@@ -4,12 +4,16 @@ import json
 from shapely.geometry import Point
 import os
 
-traffic_data='data/concatenated_data.jsonl'
-with open(traffic_data, 'r') as file:
-    for line in file:
-        traffic_data = [json.loads(line.strip())]
+# Chemin vers les données de trafic
+traffic_data_path = 'data/concatenated_data.jsonl'
 
-# Valider les données pour s'assurer que les coordonnées sont valides (Souvent des problemes)
+# Charger les données de trafic
+traffic_data = []
+with open(traffic_data_path, 'r') as file:
+    for line in file:
+        traffic_data.append(json.loads(line.strip()))
+
+# Valider les données pour s'assurer que les coordonnées sont valides
 valid_traffic_data = [
     item for item in traffic_data
     if item.get('location')
@@ -20,7 +24,7 @@ valid_traffic_data = [
     and item['location']['coordinates'][1] is not None
 ]
 
-#GeoDataFrame
+# Création du GeoDataFrame
 traffic_gdf = gpd.GeoDataFrame(
     valid_traffic_data,
     geometry=[Point(data['location']['coordinates']) for data in valid_traffic_data],
@@ -34,8 +38,9 @@ routes_gdf = routes_gdf.to_crs(traffic_gdf.crs)
 # Effectuer une jointure spatiale
 joined_gdf = gpd.sjoin_nearest(routes_gdf, traffic_gdf, how="inner", max_distance=100)
 
-# Fonction couleur des routes
+# Fonction pour déterminer la couleur en fonction de l'intensité
 def get_color(intensity):
+
     """
     Détermine une couleur en fonction de l'intensité donnée.
 
@@ -57,18 +62,19 @@ def get_color(intensity):
     """
     if intensity > 2000:
         return 'darkred'
-    elif intensity > 1000:
+    elif intensity > 1000 and intensity <= 2000:
         return 'red'
-    elif intensity > 500:
+    elif intensity > 500 and intensity <= 1000:
         return 'darkorange'
-    elif intensity > 250:
+    elif intensity > 250 and intensity <= 500:
         return 'gold'
     else:
         return 'green'
 
-# Générer une carte Folium
+# Création de la carte
 map = folium.Map(location=[43.610769, 3.876716], zoom_start=13)
 
+# Ajout des routes à la carte avec la couleur correspondant à l'intensité
 for _, row in joined_gdf.iterrows():
     if row.geometry and row.geometry.geom_type == 'LineString':
         coords = [[p[1], p[0]] for p in row.geometry.coords]
@@ -78,7 +84,7 @@ for _, row in joined_gdf.iterrows():
             weight=5
         ).add_to(map)
 
-
+# Sauvegarder la carte
 output_dir = 'map'
 os.makedirs(output_dir, exist_ok=True)
 map.save(os.path.join(output_dir, 'ecoCompteurRoutes.html'))
